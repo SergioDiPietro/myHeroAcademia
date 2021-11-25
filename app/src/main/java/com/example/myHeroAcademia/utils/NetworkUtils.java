2dda5d624bc4fb415e662bbace5c36eb82310cf0
Sergio Di Pietro <usr2264@salesianos-lacuesta.net>
2021-11-25 21:41:08 +0000

    Feat: Generación de URL para consultar un héroe en la API pasando un ID

--- app/src/main/java/com/example/myHeroAcademia/utils/NetworkUtils.java
+++ app/src/main/java/com/example/myHeroAcademia/utils/NetworkUtils.java
@@ -8,6 +8,8 @@ import java.net.URL;
 public class NetworkUtils {
     final static String BASE_URL = "https://akabab.github.io/superhero-api/api";
     final static String ALL_HEROES_PARAM = "/all.json";
+    final static String SINGLE_HERO_PARAM = "id";
+    final static String HERO_ID = "*.json";
 
     public static URL generateUrl() {
         Uri buildUri = Uri.parse(BASE_URL + ALL_HEROES_PARAM).buildUpon().build();
@@ -20,4 +22,19 @@ public class NetworkUtils {
         }
         return url;
     }
+
+    public static URL generateUrl(char heroID) {
+        Uri buildUri = Uri.parse(BASE_URL).buildUpon()
+                .appendQueryParameter(SINGLE_HERO_PARAM, HERO_ID.replace('*', heroID))
+                .build();
+        URL url = null;
+        try {
+            url = new URL(buildUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+
 }
