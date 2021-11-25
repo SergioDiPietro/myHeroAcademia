2246df364daf86f9f05389ab11f3d1f9585f795f
Sergio Di Pietro <usr2264@salesianos-lacuesta.net>
2021-11-25 21:37:09 +0000

    Feat: Generación de URL para consultar todos los héroes en la API

--- app/src/main/java/com/example/myHeroAcademia/utils/NetworkUtils.java
+++ app/src/main/java/com/example/myHeroAcademia/utils/NetworkUtils.java
@@ -1,5 +1,23 @@
 package com.example.myHeroAcademia.utils;
 
-public class NetworkUtils {
+import android.net.Uri;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class NetworkUtils {
+    final static String BASE_URL = "https://akabab.github.io/superhero-api/api";
+    final static String ALL_HEROES_PARAM = "/all.json";
+
+    public static URL generateUrl() {
+        Uri buildUri = Uri.parse(BASE_URL + ALL_HEROES_PARAM).buildUpon().build();
+
+        URL url = null;
+        try {
+            url = new URL(buildUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
 }
